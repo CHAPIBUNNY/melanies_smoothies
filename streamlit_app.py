59b44e10d159ee662bbdad6a43ccc96af4dcf07d
Charl Van Zyl <162579999+CHAPIBUNNY@users.noreply.github.com>
2024-12-16 12:58:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,7 +55,6 @@ if time_to_insert:
 # Additional logic to fetch and display fruit data
 if ingredients_list:
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{fruit_chosen.lower()}")
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
